779b3e03fe6ed56b6bf4fdef47de996254bf866f
Hüseyin Can <62290639+huseyincanersan@users.noreply.github.com>
2022-06-15 23:10:22 +0300

    c# kampi ilk dosyalar commit edildi

---
+++ MyConsoleApp/MyConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MyConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Circle c1 = new Circle();
+            c1.radius = 3.14;
+
+
+            Console.WriteLine("c1 Yari cap degeri= {0}", c1.radius);
+            
+        }
+    }
+}
